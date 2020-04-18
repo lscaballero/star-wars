61963889c0ff74b998f217daafb524da5b384447
Alfredo <alfreddocaballero@gmail.com>
2020-04-18 16:42:40 -0500

    se crea el componente people y su hijo people-internal

---
+++ src/app/people/people-internal/people-internal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-people-internal',
+  templateUrl: './people-internal.component.html',
+  styleUrls: ['./people-internal.component.css']
+})
+export class PeopleInternalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
