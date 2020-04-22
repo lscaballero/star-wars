2b52fd7c7a60809b687ad0cc63e3e0083479cd2c
Alfredo <alfreddocaballero@gmail.com>
2020-04-21 19:05:49 -0500

    se actualizan los nombres de los compnentes creados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -7,18 +8,25 @@ import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
 import { UsuariosComponent } from './usuarios/usuarios.component';
 import { StarshipsComponent } from './starships/starships.component';
+import { HomeComponent } from './home/home.component';
+import { PeopleComponent } from './people/people.component';
+import { PeopleInternalComponent } from './people/people-internal/people-internal.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     UsuariosComponent,
-    StarshipsComponent
+    StarshipsComponent,
+    HomeComponent,
+    PeopleComponent,
+    PeopleInternalComponent
     
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
