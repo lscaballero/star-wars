9834f16fa8a41d78fa5ec6598492a725531c9ed5
Alfredo <alfreddocaballero@gmail.com>
2020-04-25 15:07:45 -0500

    se crea las rutas a los diferentes componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,34 @@ import { Routes, RouterModule } from '@angular/router';
 import { PeopleComponent } from './people/people.component';
 import { StarshipsComponent } from './starships/starships.component';
 import { HomeComponent } from './home/home.component';
+import { FilmsComponent } from './films/films.component';
 //import { VehiclesComponent } from './vehicles/vehicles.component'; // actualmente con conflictos
 
 
 const routes: Routes = [
+  /**
+   *   RUTAS COMPONENTES , VISTAS INICIALES
+   */
+
+  
   {path: 'starships', component: StarshipsComponent},
   {path: 'people', component: PeopleComponent },
+  {path: 'films', component: FilmsComponent},
   // {path: 'vehicles', component: VehiclesComponent}, // actualmente con conflictos
+
+  /**
+   * RUTAS COMPONENTES , VISTAS INTERNAS
+   */
+
+
+   /**
+    * RUTAS AL HOME
+    */
+
   {path: '', component: HomeComponent },// ruta al home
   {path: '**', component: HomeComponent}, // envia al home si no encuentra la ruta
   //{path: '', redirecTo:'/home', pathMatch: 'full'}
+
 
 ];
 
