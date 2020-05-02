24b3e487179bc7fc9786930c43107e75b4215dd8
Alfredo <alfreddocaballero@gmail.com>
2020-05-01 22:39:01 -0500

    se crea la funcion para el servicio people

--- src/app/people/people.component.ts
+++ src/app/people/people.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { PeliculasService } from '../services/peliculas.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-people',
@@ -10,13 +11,13 @@ export class PeopleComponent implements OnInit {
 
   listPeople = [];
 
-  constructor(private people: PeliculasService) {
+  constructor(private people: PeliculasService, private router: Router) {
 
     this.listPeople = [];
     this.getListOfPeople();
 
    }
-
+//funcion para consumir el servicio
    getListOfPeople(){
 
     this.people.getAllPeople().subscribe(
@@ -29,5 +30,6 @@ export class PeopleComponent implements OnInit {
 
   ngOnInit(): void {
   }
+  
 
 }
